6357b19741adf2793ba383a3743b55ba2a5c05a5
Lindani Phiri <lphiri@redhat.com>
2013-09-20 14:54:38 -0400

    Initial code

--- lib/red_hat_access/navigation/articles.rb
+++ lib/red_hat_access/navigation/articles.rb
@@ -17,7 +17,7 @@ module RedHatAccess
 
       def initialize()
         @key           = :articles
-        @display       = _("Red Hat Articles")
+        @display       = _("Red Hat Access")
         @authorization = lambda{true}
         @url           = red_hat_access_articles_path
       end
