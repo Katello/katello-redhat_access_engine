93eb3049a13f6537024ae45afe28a44f812b5d46
Tom McKay <thomasmckay@redhat.com>
2013-10-07 15:38:51 -0400

    banner-menu - add menu to banner, left of user menu
    banner-menu - clean up whitespace

--- lib/red_hat_access/engine.rb
+++ lib/red_hat_access/engine.rb
@@ -11,26 +11,26 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 module RedHatAccess
-	class Engine < ::Rails::Engine
+  class Engine < ::Rails::Engine
 
-		initializer :finisher_hook do |engine|
-			resources = Dir[File.dirname(__FILE__) + '/navigation/*.rb']
-			resources.uniq.each{ |f| require f }
+    initializer :finisher_hook do |engine|
+      resources = Dir[File.dirname(__FILE__) + '/navigation/*.rb']
+      resources.uniq.each{ |f| require f }
 
-			::Navigation::Additions.insert_after(:organizations, RedHatAccess::Navigation::Articles)
-		end
+      ::Navigation::Additions.insert_before(:user, RedHatAccess::Navigation::Articles)
+    end
 
-		initializer :register_assets do |engine|
+    initializer :register_assets do |engine|
 
-			if Rails.env.production?
-				assets = YAML.load_file("#{RedHatAccess::Engine.root}/public/assets/manifest.yml")
+      if Rails.env.production?
+        assets = YAML.load_file("#{RedHatAccess::Engine.root}/public/assets/manifest.yml")
 
-				assets.each_pair do |file, digest|
-					engine.config.assets.digests[file] = digest
-				end
-			end
+        assets.each_pair do |file, digest|
+          engine.config.assets.digests[file] = digest
+        end
+      end
 
-			engine.middleware.use ::ActionDispatch::Static, "#{RedHatAccess::Engine.root}/public"
-		end
-	end
+      engine.middleware.use ::ActionDispatch::Static, "#{RedHatAccess::Engine.root}/public"
+    end
+  end
 end
